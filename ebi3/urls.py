6efe0b2d5709b5e41e0d8a3a0a86f90f9829c43c
fathi saidi <fathi.saidi@gmail.com>
2025-11-16 16:27:16 +0100

    Ajout d'une vue de bienvenue a la racine pour le test de deploiement.

--- ebi3/urls.py
+++ ebi3/urls.py
@@ -1,24 +1,14 @@
-"""
-URL configuration for ebi3 project.
+# ebi3/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
+from django.http import HttpResponse # <-- AJOUTEZ CETTE LIGNE
+
+def welcome_view(request): # <-- AJOUTEZ CETTE FONCTION
+    return HttpResponse("Bienvenue sur le projet ebi3 ! Base en cours de construction.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('annonces/', include('annonces.urls')),
+    path('', welcome_view, name='home'), # <-- AJOUTEZ CETTE LIGNE
 ]
